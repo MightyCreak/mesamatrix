df656f51b05d7fe749458dce0a77ea8f8aa9b311
Creak <romain.failliot@foolstep.com>
2025-07-26 12:21:33 -0400

    fix: use a full CSS solution for the "Fork me on GitHub" ribbon

--- README.md
+++ README.md
@@ -117,6 +117,8 @@ Mesamatrix is available under the AGPLv3, a copy of which is available in the
 * [jQuery Tipsy](http://onehackoranother.com/projects/jquery/tipsy/) is
   available under the MIT License.
 * PSR Log is available under the MIT License.
+* [Fork me on GitHub](https://simonwhitaker.github.io/github-fork-ribbon-css/)
+  is available under the MIT License.
 * [Symfony](https://symfony.com/) is available under the MIT License.
 
 ### Media files
@@ -127,5 +129,3 @@ Go tweak it, and send us your improvements!
 
 The RSS feed icon is freely available from the Mozilla Foundation at
 <http://www.feedicons.com/>.
-
-The GitHub 'Fork me' ribbon is available under the MIT license.
--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -36,6 +36,7 @@ abstract class BaseController
 
     public function __construct()
     {
+        $this->addCssScript('css/gh-fork-ribbon.css');
         $this->addCssScript('css/style.css?v=' . (Mesamatrix::$config->getValue("info", "version")));
 
         $this->addJsScript('js/jquery-3.7.0.min.js');
@@ -189,9 +190,7 @@ HTML;
         echo <<<HTML
         </main>
         <footer>
-            <a id="github-ribbon" href="{$projectUrl}">
-                <img src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" />
-            </a>
+            <a class="github-fork-ribbon" href="{$projectUrl}" data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>
             <div class="theme-switch-wrapper">
                 <label class="theme-switch" for="checkbox">
                     <input type="checkbox" id="checkbox" />
